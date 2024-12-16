b6552c78be8f57f4bcc8daef31c343ebdaa9f29b
SyuxCodeZ <cuberdytm@gmail.com>
2024-12-16 15:26:49 +0600

    activity 2 complete

---
+++ Activity2.py
@@ -0,0 +1,21 @@
+# the variables
+
+age = 12
+name = "sidratul"
+weight = 43.5
+is_student = True
+
+print(age)
+print(name)
+print(weight)
+print(is_student)
+
+# After Typecasting...
+
+age = float(age)
+print(age)
+print("the data type of age is", type(age))
+
+weight = int(weight)
+print(weight)
+print("the data type of weight is", type(weight))
